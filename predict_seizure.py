70c6b1899d1884c89c668bb6b29d43af38293086
Swathi Baskar <swathiibaskarr@gmail.com>
2025-08-24 21:24:44 +0530

    Initial commit

--- predict_seizure.py
+++ predict_seizure.py
@@ -2,10 +2,10 @@ import pickle
 import numpy as np
 import tensorflow as tf
 
-# Load the trained model
+# Loading the trained model
 model = tf.keras.models.load_model("seizure_model.h5")  # Load from .h5
 
-# Load the scaler
+# Loading the scaler
 with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
@@ -20,8 +20,8 @@ def predict_seizure(eeg_data):
     Returns:
         float: Probability of seizure (0 to 1).
     """
-    eeg_data = np.array(eeg_data).reshape(1, -1)  # Reshape for model input
-    scaled_data = scaler.transform(eeg_data)  # Apply the same scaling as training
-    probability = model.predict(scaled_data)[0][0]  # Get probability of class 1 (seizure)
+    eeg_data = np.array(eeg_data).reshape(1, -1)  # Reshaping for model input
+    scaled_data = scaler.transform(eeg_data)  # Applying the same scaling as training
+    probability = model.predict(scaled_data)[0][0]  # Getting probability of class 1 (seizure)
     return probability
 
